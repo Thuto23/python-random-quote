bdb316fedc5dd045b233e1d8e9ec0f9155ec7b80
Thuto23 <87589690+Thuto23@users.noreply.github.com>
2021-07-18 04:26:28 +0200

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,6 +1,6 @@
 def main():
-  # print("Keep it logically awesome.")
-
+  print("Keep it logically awesome.")
+python get-qoute.py python3 get-qoute.py
   #f = open("quotes.txt")
   #quotes = f.readlines()
   #f.close()
